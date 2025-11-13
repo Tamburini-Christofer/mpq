e0449485faafe719533e96f26fcca251694cfb76
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-11-13 18:13:54 +0100

    prima creazione navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,23 @@
-function NavBar () {
+function NavBar() {
   return (
-    <>
-    </>
-  )
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="./img/" alt="Logo" className="logo-icon" />
+      </div>
+
+      <ul className="navbar-links">
+        <li><a href="/shop">Shop</a></li>
+        <li><a href="/categories">Categories</a></li>
+        <li><a href="/daily-quests">Daily Quests</a></li>
+      </ul>
+
+      <div className="navbar-actions">
+        <span className="cart-icon">🛒</span> 
+        
+        <button className="btn-levelup">Level Up!</button>
+      </div>
+    </nav>
+  );
 }
+
 export default NavBar;
