29fa538fcb3c011b0661bd4ea013de7ed9d8e0ed
samir <samirsadriuboolean@gmail.com>
2025-11-18 21:32:44 +0100

    Bozza Error 404

--- src/pages/NotFoundPages.jsx
+++ src/pages/NotFoundPages.jsx
@@ -1,6 +1,18 @@
+import "./NotFoundPages.css"
+import NotFoundImg from "/logo/404.jpeg";
+import { Link } from "react-router-dom";
+
 function NotFoundPages() {
     return (
-        <>
+        <>  
+            <div className="notfound">
+                <div className="notfoundbox">
+                <h1>Devi aver tirato l'incantesimo sbagliato!</h1>
+                <img src={NotFoundImg} alt="not_found" />
+                <Link to={'/'}><button>Torna alla home!</button></Link>
+                </div>
+            </div>
+
         </>
     )
 }
